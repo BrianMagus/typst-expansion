typst/typst

feat: make pdf_to_svg public

crates/typst-svg/src/lib.rs
@@ -5,7 +5,7 @@ mod paint;
 mod shape;
 mod text;
 
-pub use image::{convert_image_scaling, convert_image_to_base64_url};
+pub use image::{convert_image_scaling, convert_image_to_base64_url, pdf_to_svg};
 use rustc_hash::FxHashMap;
 use typst_library::introspection::Introspector;
 use typst_library::model::Destination;

BrianMagus/typst-expansion

crates/typst-svg/src/lib.rs
@@ -8,7 +8,7 @@ mod text;
 mod write;
 
 use comemo::Tracked;
-pub use image::{WebImage, convert_image_scaling};
+pub use image::{WebImage, convert_image_scaling, pdf_to_svg};
 use indexmap::IndexMap;
 use rustc_hash::FxBuildHasher;
 use typst_library::model::{Destination, LateLinkResolver};